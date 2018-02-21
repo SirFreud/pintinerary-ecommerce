15b008be2822e4d0dfe629f11fe1bbaa7839c588
Sirfreud <mikevaldez92@gmail.com>
2018-02-21 03:08:19 -0600

    Made Itinerary model and copied migration file from pintinerary-basic. Added sluggable and trait+methods to Itinerary model

---
+++ app/Itinerary.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Itinerary extends Model
+{
+    use Sluggable, SoftDeletes;
+    
+    /**
+    * Get the route key for the model.
+    *
+    * @return string
+    */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+      /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
