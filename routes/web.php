29ff3a5b6b63446d8827c925bd488ae2863ba5d9
Sirfreud <mikevaldez92@gmail.com>
2018-02-20 20:49:30 -0600

    Added partials folder and moved _head and _nav into it. Also made ItineraryController and added first /itinerary route to it. Used bulma preset and made auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/itinerary', 'ItineraryController@index')->name('itinerary.index');
