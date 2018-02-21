48a39c934640f8147f55d42fc502997c10273070
Sirfreud <mikevaldez92@gmail.com>
2018-02-21 02:37:55 -0600

    Added unsplash photos from source API

--- resources/views/itinerary/index.blade.php
+++ resources/views/itinerary/index.blade.php
@@ -5,14 +5,14 @@
         <div class="container">
             <div class="tile is-ancestor">
                 <div class="columns is-multiline" style="width: 100%">
-                    @for ($i = 0; $i < 23; $i++)
+                    @for ($i = 0; $i < 4; $i++)
                         <div class="column is-one-third">
                             <div class="tile is-12 is-parent">
                                 <article class="tile is-child notification is-primary box">
                                     <p class="title">Vertical...</p>
                                     <p class="subtitle">Top tile</p>
                                     <figure class="image is-4by3">
-                                        <img src="https://bulma.io/images/placeholders/640x480.png">
+                                        <img src="https://source.unsplash.com/8manzosDSGM/640x480">
                                     </figure>
                                 </article>
                             </div>
