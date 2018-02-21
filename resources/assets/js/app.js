c56f4801308b060097ba19e09efe67fb6e3a4986
Sirfreud <mikevaldez92@gmail.com>
2018-02-21 02:29:12 -0600

    Built wrapping tile grid with bulma. All working in itinerary.index view

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('tile-component', require('./components/TileComponent.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/itinerary/index.blade.php
+++ resources/views/itinerary/index.blade.php
@@ -4,39 +4,23 @@
     <section class="section">
         <div class="container">
             <div class="tile is-ancestor">
-                <div class="tile is-vertical is-8">
-                    <div class="tile">
-                        <div class="tile is-parent is-vertical">
-                            <article class="tile is-child notification is-primary box">
-                                <p class="title">Vertical...</p>
-                                <p class="subtitle">Top tile</p>
-                                <figure class="image is-4by3">
-                                    <img src="https://bulma.io/images/placeholders/640x480.png">
-                                </figure>
-                            </article>
-                            
+                <div class="columns is-multiline" style="width: 100%">
+                    @for ($i = 0; $i < 23; $i++)
+                        <div class="column is-one-third">
+                            <div class="tile is-12 is-parent">
+                                <article class="tile is-child notification is-primary box">
+                                    <p class="title">Vertical...</p>
+                                    <p class="subtitle">Top tile</p>
+                                    <figure class="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/640x480.png">
+                                    </figure>
+                                </article>
+                            </div>
                         </div>
-                        <div class="tile is-parent">
-                            <article class="tile is-child notification is-info box">
-                                <p class="title">Middle tile</p>
-                                <p class="subtitle">With an image</p>
-                                <figure class="image is-4by3">
-                                    <img src="https://bulma.io/images/placeholders/640x480.png">
-                                </figure>
-                            </article>
-                        </div>
-                    </div>
-                </div>
-                <div class="tile is-parent">
-                    <article class="tile is-child notification is-success box">
-                        <p class="title">Tall tile</p>
-                        <p class="subtitle">With even more content</p>
-                        <figure class="image is-4by3">
-                            <img src="https://bulma.io/images/placeholders/640x480.png">
-                        </figure>
-                    </article>
+                    @endfor
                 </div>
             </div>
         </div>
     </section>
+
 @endsection
